3048c9e1f4c2b5da5c06003f5829a2d45b3b947a
Robert <robyop.eyeco@gmail.com>
2021-11-22 16:14:01 +0700

    update viewTableModal

--- orderAhead-FE/src/views/admin/ViewItemDB.js
+++ orderAhead-FE/src/views/admin/ViewItemDB.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import {
     CModal,
@@ -9,19 +9,32 @@ import {
     CCardHeader,
     CCol,
     CRow,
+    CTextarea,
   } from '@coreui/react'
 
 const ViewItemDB = () => {
   const dispatch = useDispatch()
 
   const viewItemDB = useSelector(state => state.viewItemDB)
-  const viewDetail = useSelector(state => state.viewDetail)
+  const originData = useSelector(state => state.viewDetail)
   const viewColumn = useSelector(state => state.viewColumn)
   const selectedTable = useSelector(state => state.selectedTable)
+
+  const [viewDetail, setViewDetail] = useState(originData)
+
+  useEffect(() => {
+      console.log(originData)
+      setViewDetail(originData)
+  }, [originData])
 
   const handleClose = () => {
     dispatch({type: 'set', viewItemDB: false})
   };
+
+  const updateDetailItem = (value, index) => {
+      originData[index] = value
+      dispatch({type: 'set', viewDetail: originData})
+  }
 
   return (
     <CModal 
@@ -37,11 +50,18 @@ const ViewItemDB = () => {
                     viewDetail.map((item, index) => (
                         <CCol xs="12" sm="12" md="12" className="m-0 p-0 px-1">
                             <CCard className="m-0">
-                                <CCardHeader className="view-table-header bg-info">
+                                <CCardHeader className="view-table-header">
                                     {viewColumn[index]}
                                 </CCardHeader>
                                 <CCardBody className="view-table-content">
-                                    {item}
+                                    { viewColumn[index] != 'insert_datetime' ?
+                                        (
+                                            !viewDetail[index] || viewDetail[index].indexOf("/", 1) != 2?
+                                                <CTextarea className="m-0 p-0 px-1 view-table-content" value={viewDetail[index]} onChange={(e) => updateDetailItem(e.target.value, index)} ></CTextarea>
+                                            : item
+                                        )
+                                    : item
+                                    }
                                 </CCardBody>
                             </CCard>
                         </CCol>
@@ -50,6 +70,10 @@ const ViewItemDB = () => {
             </CRow>
             
             <div className="d-flex mx-3 px-3 mt-2">
+                <CButton block className="button-exchange p-1 pt-2" onClick={handleClose}>
+                    <h3 style={{fontSize: "medium"}}>Update</h3>
+                </CButton>
+
                 <CButton block className="button-exchange p-1 pt-2" onClick={handleClose}>
                     <h3 style={{fontSize: "medium"}}>Close</h3>
                 </CButton>
